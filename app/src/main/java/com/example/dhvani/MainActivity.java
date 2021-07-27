7692151b9931493dc5ab03cdf61e88d5c2822e0c
shasin28 <shalini28sss@gmail.com>
2021-07-28 04:31:15 +0530

    second commit with improvisation

--- app/src/main/java/com/example/dhvani/MainActivity.java
+++ app/src/main/java/com/example/dhvani/MainActivity.java
@@ -2,72 +2,51 @@ package com.example.dhvani;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     BottomNavigationView bottomNavigationV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationV=findViewById(R.id.bottomNavigationView);
+     //   bottomNavigationV.setSelectedItemId(R.id.home);
         bottomNavigationV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId())
+                switch(item.getItemId())
                 {
-                    R.id.home:
-                    Toast.makeText(this,"Home",Toast.LENGTH_SHORT).show();
-                    break;
-                    R.id.record:
-                    Toast.makeText(this,"Record",Toast.LENGTH_SHORT).show();
-                    break;
-                    R.id.find:
-                    Toast.makeText(this,"Find",Toast.LENGTH_SHORT).show();
-                    break;
+                   /* case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;*/
+                    case R.id.home:
+                       // Toast.makeText(MainActivity.this, "Record", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(R.anim.leftanimation,R.anim.rightanimation);
+                        break;
+                    case R.id.record:
+                        // Toast.makeText(MainActivity.this, "Record", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),RecordActivity.class));
+                        overridePendingTransition(R.anim.leftanimation,R.anim.rightanimation);
+                        break;
+                    case R.id.find:
+                        //Toast.makeText(MainActivity.this, "Find", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),FindActivity.class));
+                        overridePendingTransition(R.anim.leftanimation,R.anim.rightanimation);
+                        break;
 
-
-                  //  break;
-                  //  default:
-                     //  throw new IllegalStateException("Unexpected value: " + item.getItemId());
                 }
                 return true;
             }
         });
-      /*  buttton=findViewById(R.id.button);
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO)!= PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},1);
-        }*/
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==1)
-        {
-            if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
-            {
-                Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT);
-            }
-            else
-            {
-                Toast.makeText(this,"Permission Denied",Toast.LENGTH_SHORT);
-            }
-        }
     }
 }
--- app/src/main/java/com/example/dhvani/RecordActivity.java
+++ app/src/main/java/com/example/dhvani/RecordActivity.java
@@ -1,14 +1,51 @@
 package com.example.dhvani;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class RecordActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+      //  setContentView(R.layout.activity_find);
+       // setContentView(R.layout.activity_main);
+        bottomNavigationV=findViewById(R.id.bottomNavigationView);
+       // bottomNavigationV.setSelectedItemId(R.id.home);
+        bottomNavigationV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                   /* case R.id.home:
+                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                        break;*/
+                    case R.id.home:
+                        // Toast.makeText(MainActivity.this, "Record", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(R.anim.leftanimation,R.anim.rightanimation);
+                        break;
+                    case R.id.record:
+                        // Toast.makeText(MainActivity.this, "Record", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),RecordActivity.class));
+                        overridePendingTransition(R.anim.leftanimation,R.anim.rightanimation);
+                        break;
+                    case R.id.find:
+                        //Toast.makeText(MainActivity.this, "Find", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(),FindActivity.class));
+                        overridePendingTransition(R.anim.leftanimation,R.anim.rightanimation);
+                        break;
+
+                }
+                return true;
+            }
+        });
     }
 }
